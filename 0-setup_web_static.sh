16200aafcf6bc9548110e4e1441469c1152c40ca
TrieToSucceed <jian.huang.8581@gmail.com>
2019-01-14 21:41:09 +0000

    add feature: script now restarts nginx after modifying config files

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,5 +13,7 @@ sudo mkdir -p /data/web_static/shared
 sudo echo -e "Hello World" | sudo tee "/data/web_static/releases/test/index.html"
 sudo ln -sf "/data/web_static/current" "/data/web_static/releases/test/"
 
+# change owners and alter config file, then restart nginx
 sudo chown -R ubuntu:ubuntu "/data/"
 sudo sed -i "29i \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}" "/etc/nginx/sites-available/default"
+sudo service nginx restart
