3312de851ebd888a0de41d6539c8103eda13e99f
TrieToSucceed <jian.huang.8581@gmail.com>
2019-01-14 21:35:11 +0000

    initial commit: install nginx and create necessary folders for deployment of code

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Install nginx and prepare web server for static code deloyment
+
+# nginx installation
+sudo apt-get update
+sudo apt-get -y install nginx
+
+# create necessary directories
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+
+# create index.html and symlink
+sudo echo -e "Hello World" | sudo tee "/data/web_static/releases/test/index.html"
+sudo ln -sf "/data/web_static/current" "/data/web_static/releases/test/"
+
+sudo chown -R ubuntu:ubuntu "/data/"
+sudo sed -i "29i \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}" "/etc/nginx/sites-available/default"
