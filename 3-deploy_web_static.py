96ddc0d652c6ffbaa852456f58db9e8e8561c447
TrieToSucceed <jian.huang.8581@gmail.com>
2019-01-15 07:11:07 +0000

    fix bug: current directory was not being deleted, but now it has been removed upon running code

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -58,7 +58,7 @@ def do_deploy(archive_path):
         run("sudo rm -rf /data/web_static/releases/{}/web_static".format(
                 file_name.split('.')[0]
             ))
-        run("sudo rm -rf /data/web_static/current/")
+        run("sudo rm -rf /data/web_static/current")
         run("sudo ln -s /data/web_static/releases/{}"
             " /data/web_static/current/".format(
                 file_name.split('.')[0]
