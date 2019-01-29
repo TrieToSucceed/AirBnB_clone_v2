56b976204597fe89ec060c8cff8fbc5c150e565d
TrieToSucceed <jian.huang.8581@gmail.com>
2019-01-29 20:49:58 +0000

    initial commit: add route handler that prints string

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application listening on port 5000
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def print_hello():
+    """
+    print hello
+    """
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run()
