9b1c5ae3d7224f762efbc135960a1c50f852044a
TrieToSucceed <jian.huang.8581@gmail.com>
2019-01-29 20:50:50 +0000

    initial commit: add route handler that dynamically prints string based on route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application listening on port 5000
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def print_hello():
+    """
+    print hello
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def print_hbnb():
+    """
+    print hbnb
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def print_c_text(text):
+    """
+    print c with passed in value
+    """
+    text = text.replace("_", " ")
+    return "c %s" % text
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def print_python_text(text="is cool"):
+    """
+    print python with passed in value
+    """
+    text = text.replace("_", " ")
+    return "python %s" % text
+
+if __name__ == "__main__":
+    app.run()
